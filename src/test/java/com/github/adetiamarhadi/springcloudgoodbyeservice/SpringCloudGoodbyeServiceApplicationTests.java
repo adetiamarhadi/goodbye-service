41d837bff1b15f13ea974ba1f36fc02e06e60a26
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-16 17:56:40 +0700

    simple goodbye webservice

---
+++ src/test/java/com/github/adetiamarhadi/springcloudgoodbyeservice/SpringCloudGoodbyeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springcloudgoodbyeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGoodbyeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
